95d44dc491572ce5f6221d2b71bce87aa4877b87
Tuna <148794629+Tunna6@users.noreply.github.com>
2023-12-14 13:50:12 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,21 +3,21 @@ let urunler = [
         "isim": "Waffle Pike Takımı",
         "açıklama": "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "fiyat": 79.99,
-        "fotoğraf": "/css/img/ce69e0_bd3d06e4b2634f0fa4cee503ea28a751~mv2.webp",
+        "fotoğraf": "./css/img/ce69e0_bd3d06e4b2634f0fa4cee503ea28a751~mv2.webp",
         "stok": 150
     },
     {
         "isim": "Pike Takımı",
         "açıklama": "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "fiyat": 129.99,
-        "fotoğraf": "/css/img/451732_6a0cc8c520d94f3996a8e9e85590c7bc~mv2.webp",
+        "fotoğraf": "./css/img/451732_6a0cc8c520d94f3996a8e9e85590c7bc~mv2.webp",
         "stok": 100
     },
     {
         "isim": "Rose Çift Kişilik Pike Takımı",
         "açıklama": "Some quick example text to build on the card title and make up the bulk of the card's content.",
         "fiyat": 199.99,
-        "fotoğraf": "/css/img/ce69e0_8f42bb260f024ae49946ebb80255f64c~mv2 (1).webp",
+        "fotoğraf": "./css/img/ce69e0_8f42bb260f024ae49946ebb80255f64c~mv2 (1).webp",
         "stok": 75
     },
 ]
